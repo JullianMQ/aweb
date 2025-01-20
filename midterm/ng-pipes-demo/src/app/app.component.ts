04de52382ef4701ecde802aa2fa38e96c6bd2af6
JullianMQ <120291086+JullianMQ@users.noreply.github.com>
2025-01-20 11:21:27 +0800

    ng service

--- midterm/ng-pipes-demo/src/app/app.component.ts
+++ midterm/ng-pipes-demo/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from "./components/test/test.component";
+import { MyserviceService } from './myservice.service';
+import { NewCmpComponent } from "./new-cmp/new-cmp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, TestComponent, NewCmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'midterm1';
+  title = 'midterm1'
+  ndate
+  componentproperty
+  constructor(private myservice:MyserviceService) {
+    this.ndate = this.myservice.showTodayDate()
+    this.componentproperty = this.myservice.serviceproperty
+  }
 }
